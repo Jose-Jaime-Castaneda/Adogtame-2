4269101c7f8a4b7e6c39e5a5563bf4ac2b64d501
Erick Ramón Hernández Ortega <47erickortega@gmail.com>
2023-09-07 20:50:20 -0600

    Se crea el menu de TabBottom

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Prelogin from './src/components/Prelogin/Prelogin'
 import CargaInicial from './src/components/Prelogin/CargaInicial'
+import { NavigationContainer } from '@react-navigation/native';
+import BottomTab from './src/components/navigation/BottomTab';
 
 
 function App(): JSX.Element {
   return(
-    <Prelogin />
+    // <Prelogin />
+
+    <NavigationContainer>
+      <BottomTab/>
+    </NavigationContainer>
   );
 }
 
