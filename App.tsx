a89188b36db1c0b0b09c81fcb1a05be8ec9835e7
Erick Ramón Hernández Ortega <47erickortega@gmail.com>
2023-09-13 15:45:28 -0600

    Borramos importaciones no necesarias

--- App.tsx
+++ App.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Prelogin from './src/components/Prelogin/Prelogin'
-import CargaInicial from './src/components/Prelogin/CargaInicial'
 import { NavigationContainer } from '@react-navigation/native';
 import BottomTab from './src/components/navigation/BottomTab';
 
