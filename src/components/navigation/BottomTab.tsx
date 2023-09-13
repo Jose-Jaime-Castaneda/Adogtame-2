c51919e96300f01eeb060392590e0d2bb2d183d2
Erick Ramón Hernández Ortega <47erickortega@gmail.com>
2023-09-13 15:38:12 -0600

    Se cambia de menu a uno mas bonito

--- src/components/navigation/BottomTab.tsx
+++ src/components/navigation/BottomTab.tsx
@@ -1,18 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import CargaInicial from '../Prelogin/CargaInicial';
 import Prelogin from '../Prelogin/Prelogin';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 const BottomTab = () => {
-  return (
+  return(
     <Tab.Navigator>
-        <Tab.Screen name="Carga inicial" component={CargaInicial}/>
-        <Tab.Screen name="Prelogin" component={Prelogin}/>
+      <Tab.Screen name="Home" component={CargaInicial} />
+      <Tab.Screen name="Settings" component={Prelogin} />
     </Tab.Navigator>
-  )
+  );
 }
 
 export default BottomTab
