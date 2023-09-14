0c3e5a303dc915f4fcc8ac5c9fa5a102c52944c1
Erick Ramón Hernández Ortega <47erickortega@gmail.com>
2023-09-13 20:03:37 -0600

    Cambiamos nombre de una carpeta

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import MenuStack from './src/components/navigation/Stack';
+import MenuStack from './src/components/Navigation/Stack';
 
 
 function App(): JSX.Element {
