6fb2b4ff50de8f8fcc42fe8af1a86e09f20bfd33
Erick Ramón Hernández Ortega <47erickortega@gmail.com>
2023-10-18 19:50:06 -0600

    Se modifica nav bar bottom, ademas de crear nuevos componentes

---
+++ src/components/Cuenta/Cuenta.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Cuenta = () => {
+  return (
+    <View>
+      <Text>Cuenta</Text>
+    </View>
+  )
+}
+
+export default Cuenta
+
+const styles = StyleSheet.create({})
---
+++ src/components/Mensajes/Mensajes.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Mensajes = () => {
+  return (
+    <View>
+      <Text>Mensajes</Text>
+    </View>
+  )
+}
+
+export default Mensajes
+
+const styles = StyleSheet.create({})
