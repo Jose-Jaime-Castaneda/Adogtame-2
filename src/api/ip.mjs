c8e9d4410d1faba0869a3e65b308129ae985a786
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-11-03 15:40:22 -0600

    Cambio en la IP del servidor

--- src/api/ip.mjs
+++ src/api/ip.mjs
@@ -1,2 +1,5 @@
 // No mas cambian esta variable a su ip
-export const ip = "192.168.100.89"
+export const ip = "192.168.100.126"
+// Jaime: "192.168.100.126"
+// Ramon: "192.168.100.89"
+// Cris: ""
