31150db5452408c00417bc1f78755ef9e04b9bb6
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-11-03 15:41:09 -0600

    Obtencion de la info en el componente

--- src/components/Cuenta/Cuenta.tsx
+++ src/components/Cuenta/Cuenta.tsx
@@ -1,10 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Alert,
+} from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { getUserInformation } from '../../Utils/user.mjs'
 
 const Cuenta = () => {
+  const [userData, setUserData] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const userInformation = await getUserInformation();
+        if (userInformation) {
+          setUserData(userInformation);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+  console.log(userData);
+
   return (
     <View>
       <Text>Cuenta</Text>
+      {userData && (
+        <View>
+          <Text>Información del usuario</Text>
+          <Text>Nombre: {userData.user.Nombre}</Text>
+          <Text>Apellido: {userData.user.Apellido}</Text>
+          <Text>Alias: {userData.user.Nickname}</Text>
+          <Text>Email: {userData.user.Correo}</Text>
+          <Text>Telefono: {userData.user.Telefono}</Text>
+        </View>
+      )}
     </View>
   )
 }
