a63029106f65c571aec8b66148a8b37245a08481
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-11-03 15:40:41 -0600

    Creacion del api para obtener al usuario actual

--- src/api/server.mjs
+++ src/api/server.mjs
@@ -1,6 +1,13 @@
 import express from 'express';
 import { ip } from './ip.mjs';
-import { collection, addDoc, Timestamp } from 'firebase/firestore';
+import {
+    collection,
+    addDoc,
+    Timestamp,
+    getDocs,
+    query,
+    where
+} from 'firebase/firestore';
 import { db } from '../DB/firebase.mjs';
 
 const app = express();
@@ -28,6 +35,30 @@ app.post('/adogtame/publicaciones/', async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(400).send({ message: 'Algo fallo wacho', title: "Error" });
+    }
+});
+
+app.get('/adogtame/getUsuario/:email', async (req, res) => {
+    const email = req.params.email;
+
+    const userCollection = collection(db, 'Usuarios');
+    const queryUser = query(userCollection, where('Correo', '==', email));
+
+    const response = await getDocs(queryUser);
+
+    if (response.docs.length === 0) {
+        console.log("No existe el documento");
+        res.status(400).json({ message: 'No existe el documento', title: 'Error' });
+    } else {
+        const doc = response.docs[0];
+        if (doc.exists) {
+            const userData = doc.data();
+            //console.log("Información del usuario:", userData);
+            res.status(200).json({ message: 'Información del usuario obtenida con éxito', title: 'Éxito', user: userData });
+        } else {
+            console.log("No existe el documento");
+            res.status(400).json({ message: 'No existe el documento', title: 'Error' });
+        }
     }
 });
 
